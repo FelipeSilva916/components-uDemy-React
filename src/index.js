965a503b36d92adff68ea7cade63506bf40339da
Felipe Silva <felipesilva916@gmail.com>
2022-06-04 19:43:34 -0700

    Styles

--- src/index.js
+++ src/index.js
@@ -3,28 +3,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetails";
 import ApprovalCard from "./ApprovalCard";
+import "./style/App.css";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at this time"
-        commentText="Love this blog post"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at this time"
-        commentText="I like this blog post"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Karen"
-        timeAgo="Today at this time"
-        commentText="I hate this blog post"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at this time"
+          commentText="Love this blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at this time"
+          commentText="I like this blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Karen"
+          timeAgo="Today at this time"
+          commentText="I hate this blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
