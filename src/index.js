1bfd65e0f0893a7d67553b5e93e00442553c6432
Felipe Silva <felipesilva916@gmail.com>
2022-06-04 18:38:41 -0700

    imported Faker

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const App = () => {
           </a>
           <div className="metadata">
             <span className="date">Today at 6 pm</span>
+            <div className="text">Nice blog post!</div>
           </div>
         </div>
       </div>
